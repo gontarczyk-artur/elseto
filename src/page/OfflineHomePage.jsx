b247d0a6b218d08644e488b1fecc9fdab325528a
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2021-01-16 18:22:54 +0100

    change to function component

--- src/page/OfflineHomePage.jsx
+++ src/page/OfflineHomePage.jsx
@@ -9,49 +9,26 @@ import MenuItem from '@material-ui/core/MenuItem';
 
 import { connect } from 'react-redux';
 
+function OfflineHomePage() {
+    const [address, setAddress] = React.useState('http://localhost:9200');
+    const [version, setVersion] = React.useState(7.10);
+    const handleAddressChange = event => setAddress(event.target.value);
+    const handleVersionChange = event => setVersion(event.target.value);
+    const onConnect = () => {}
 
-class OfflineHomePage extends React.Component {
-    onConnect() {
-        //
-    }
+    return (
+        <Container fixed>
+            <Grid container spacing={0} direction="row" alignItems="center" justify="center" style={{ minHeight: '100vh' }}>
+                <TextField label="Server address" defaultValue={address} onChange={handleAddressChange} variant="outlined" size="small"  style={{'width': '300px'}} />
+                
+                <TextField select value={version} onChange={handleVersionChange} margin="normal" variant="outlined" size="small" style={{'margin': '0 20px'}}>
+                    <MenuItem key={7.10} value={7.10}>7.10</MenuItem>
+                </TextField>
 
-    render() {
-        return (
-            <Container fixed>
-                <Grid
-                    container
-                    spacing={0}
-                    direction="row"
-                    alignItems="center"
-                    justify="center"
-                    style={{ minHeight: '100vh' }}
-                >
-                    <TextField
-                        label="Server address"
-                        id="outlined-size-small"
-                        defaultValue="http://localhost:9200"
-                        variant="outlined"
-                        size="small" 
-                        style={{'width': '300px'}}
-                    />
-
-                    <TextField
-                        id="outlined-select-gender"
-                        select
-                        value={7.10}
-                        margin="normal"
-                        variant="outlined"
-                        size="small"
-                        style={{'margin': '0 20px'}}
-                    >
-                        <MenuItem key={7.10} value={7.10}>7.10</MenuItem>
-                    </TextField>
-                    
-                    <Button variant="contained" color="primary" onClick={this.onConnect}>Connect</Button>
-                </Grid>
-            </Container>
-        );
-    }
+                <Button variant="contained" color="primary" onClick={onConnect}>Connect</Button>
+            </Grid>
+        </Container>
+    )
 }
 
 const mapStateToProps = state => ({
