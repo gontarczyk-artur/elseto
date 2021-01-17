13e699c713b0eb9827361a98471ac2f4b9be8ed9
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2021-01-17 13:07:08 +0100

    use loading button component, connect to es after button click

--- src/page/OfflineHomePage.jsx
+++ src/page/OfflineHomePage.jsx
@@ -4,7 +4,7 @@ import * as React from 'react';
 import Container from '@material-ui/core/Container';
 import Grid from '@material-ui/core/Grid';
 import TextField from '@material-ui/core/TextField';
-import Button from '@material-ui/core/Button';
+import LoadingButton from '@material-ui/lab/LoadingButton';
 import MenuItem from '@material-ui/core/MenuItem';
 
 import { connect } from 'react-redux';
@@ -12,9 +12,23 @@ import { connect } from 'react-redux';
 function OfflineHomePage() {
     const [address, setAddress] = React.useState('http://localhost:9200');
     const [version, setVersion] = React.useState(7.10);
+    const [loadingButtonPending, setLoadingButtonPending] = React.useState(false);
     const handleAddressChange = event => setAddress(event.target.value);
     const handleVersionChange = event => setVersion(event.target.value);
-    const onConnect = () => {}
+    const handleConnect = async () => {
+        setLoadingButtonPending(true);
+
+        switch(version) {
+            case 7.1:
+                const { Client } = require('es-client-7.10');
+                const client = new Client({ node: address });
+                let info = await client.info();
+                if (info.statusCode === 200) {
+                    setLoadingButtonPending(false);
+                }
+                break;
+        }
+    }
 
     return (
         <Container fixed>
@@ -25,7 +39,7 @@ function OfflineHomePage() {
                     <MenuItem key={7.10} value={7.10}>7.10</MenuItem>
                 </TextField>
 
-                <Button variant='contained' color='primary' onClick={onConnect}>Connect</Button>
+                <LoadingButton variant='contained' color='primary' pending={loadingButtonPending} onClick={handleConnect}>Connect</LoadingButton>
             </Grid>
         </Container>
     )
