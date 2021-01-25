0df794147344667d7645a7dd841f8ec165a018b9
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2021-01-25 15:33:04 +0100

    on online home page, add left drawer with home icon button

--- src/page/OnlineHomePage.jsx
+++ src/page/OnlineHomePage.jsx
@@ -1,12 +1,36 @@
 'use strict';
 
 import * as React from 'react';
+import { makeStyles, useTheme } from '@material-ui/core/styles';
+import Drawer from '@material-ui/core/Drawer';
+import Tooltip from '@material-ui/core/Tooltip';
+import IconButton from '@material-ui/core/IconButton';
+import HomeIcon from '@material-ui/icons/Home';
 
 import { connect } from 'react-redux';
 
+const useStyles = makeStyles((theme) => ({
+    root: {
+        display: 'flex'
+    },
+    drawerPaper: {
+        padding: theme.spacing(1)
+    }
+}));
+
 const OnlineHomePage = props => {
+    const classes = useStyles();
+
     return props.store.isConnected ? (
-        <div></div>
+        <div className={classes.root}>
+            <Drawer variant='permanent' classes={{paper: classes.drawerPaper}}>
+                <Tooltip placement='right' title='Home'>
+                    <IconButton>
+                        <HomeIcon />
+                    </IconButton>
+                </Tooltip>
+            </Drawer>
+        </div>
     ) : null;
 }
 
