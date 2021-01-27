1c20d799184728bd76315114f2b8829dbea12091
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2021-01-27 21:10:36 +0100

    remove custom titlebar

--- src/renderer.jsx
+++ src/renderer.jsx
@@ -1,17 +1,5 @@
 'use strict';
 
-/* temporary disabled custom titlebar
-const { Menu } = require('electron').remote;
-const menu = new Menu();
-
-// Update titlebar including menu and title
-const customTitleBar = require('custom-electron-titlebar');
-const elsetoTitleBar = new customTitleBar.Titlebar({
-    backgroundColor: customTitleBar.Color.BLACK
-});
-elsetoTitleBar.updateTitle('ELSETO');
-elsetoTitleBar.updateMenu(menu);
-*/
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import { Provider } from 'react-redux';
