b18d6f3270bbf87e214017e15dee3b991dea5969
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2021-01-29 15:18:58 +0100

    remove unnused components, enable sidebar background and border color

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -3,22 +3,17 @@
 import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
-import Tooltip from '@material-ui/core/Tooltip';
 import Toolbar from '@material-ui/core/Toolbar';
-import IconButton from '@material-ui/core/IconButton';
-import DashboardIcon from '@material-ui/icons/Dashboard';
 
 const drawerWidth = 72;
 
-const useStyles = makeStyles((theme) => ({
+const useStyles = makeStyles(() => ({
     drawer: {
         width: drawerWidth,
         flexShrink: 0
     },
     drawerPaper: {
-        width: drawerWidth,
-        backgroundColor: 'transparent',
-        borderRight: 'none'
+        width: drawerWidth
     },
     drawerContainer: {
         overflow: 'auto'
