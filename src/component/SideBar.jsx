ad3d97941b2de3997e607e21ea6774834da7550a
Artur Gontarczyk <gontarczyk.artur@gmail.com>
2021-02-07 16:33:35 +0100

    convert AppBar to extra side bar

--- src/component/SideBar.jsx
+++ src/component/SideBar.jsx
@@ -4,16 +4,16 @@ import * as React from 'react';
 import { makeStyles } from '@material-ui/core/styles';
 import Drawer from '@material-ui/core/Drawer';
 import Toolbar from '@material-ui/core/Toolbar';
-
-const drawerWidth = 72;
+import MenuIcon from '@material-ui/icons/Menu';
+import IconButton from '@material-ui/core/IconButton';
 
 const useStyles = makeStyles(() => ({
     drawer: {
-        width: drawerWidth,
+        width: '72px',
         flexShrink: 0
     },
     drawerPaper: {
-        width: drawerWidth
+        width: '72px'
     },
     drawerContainer: {
         overflow: 'auto'
@@ -26,14 +26,18 @@ const SideBar = props => {
     return (
         <Drawer
             className={classes.drawer}
-            variant='persistent'
+            variant='permanent'
             anchor='left'
-            open={props.isOpen}
             classes={{
                 paper: classes.drawerPaper,
             }}
         >
-            <Toolbar />
+            <Toolbar>
+                <IconButton edge='start'>
+                    <MenuIcon />
+                </IconButton>
+            </Toolbar>
+
             <div className={classes.drawerContainer}>
                 {props.children}
             </div>
